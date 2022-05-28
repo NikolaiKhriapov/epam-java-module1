7d131646e4047369bf1d578994ec0fda950d2f0e
Nikolai Khriapov <kolyakhryapov@gmail.com>
2022-05-28 20:41:00 +0600

    adding task02

---
+++ src/conditionals/main_tasks/task02/main/Main.java
@@ -0,0 +1,23 @@
+package conditionals.main_tasks.task02.main;
+
+/*
+ * Задача 2. Найти max{min(a, b), min(c, d)}.
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 6;
+		int c = 7;
+		int d = 8;
+
+		int result = Math.max(Math.min(a, b), Math.min(c, d));
+
+		System.out.println("Число a: " + a);
+		System.out.println("Число b: " + b);
+		System.out.println("Число c: " + c);
+		System.out.println("Число d: " + d);
+		System.out.println("Результат: " + result);
+	}
+}
